507b41f93ef6966d2dee939dfc55b8f50f637538
obscurefreeman <1093048022@qq.com>
2024-12-18 14:04:42 +0800

    3D2D测试

--- lua/autorun/client/ofnpcp_levelup.lua
+++ lua/autorun/client/ofnpcp_levelup.lua
@@ -10,13 +10,17 @@ net.Receive("OFNPCRankUp", function()
     -- 在NPC上方显示文本
     local function DrawLevelUpText()
         local elapsed = CurTime() - startTime  -- 计算经过的时间
-        local pos = ent:GetPos() + Vector(0, 0, 80 + (elapsed * 50))  -- 向上移动文本，速度为50单位/秒
+        local pos = ent:GetPos() + Vector(0, 0, 40 + (elapsed * 10))  -- 向上移动文本，速度为50单位/秒
         
-        -- 计算透明度，随着时间的推移逐渐变浅
+        -- 计算透明度，随着时间的推移逐渐变浅，先慢后快
         local alpha = math.max(255 * (1 - (elapsed / textDuration)), 0)  -- 计算透明度，最大255，最小0
+        -- 调整透明度变化速度，使其先慢后快
+        alpha = 255 * (1 - (elapsed / textDuration) ^ 2)
         
         cam.Start3D2D(pos, Angle(0, LocalPlayer():EyeAngles().y - 90, 90), 0.1)
-            draw.SimpleText(levelUpText, "ofgui_medium", 0, 0, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            cam.IgnoreZ(true)
+            draw.SimpleText(levelUpText, "ofgui_eva", 0, 0, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+            cam.IgnoreZ(false)
         cam.End3D2D()
     end
 
