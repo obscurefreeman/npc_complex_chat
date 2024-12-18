da87e10f66e64aef56d88258383f0cb0f8803307
obscurefreeman <1093048022@qq.com>
2024-12-18 16:09:47 +0800

    晋升文字改为hud

--- lua/autorun/client/ofnpcp_levelup.lua
+++ lua/autorun/client/ofnpcp_levelup.lua
@@ -10,26 +10,23 @@ net.Receive("OFNPCRankUp", function()
     -- 在NPC上方显示文本
     local function DrawLevelUpText()
         local elapsed = CurTime() - startTime  -- 计算经过的时间
-        local pos = ent:GetPos() + Vector(0, 0, 40 + (elapsed * 10))  -- 向上移动文本，速度为50单位/秒
+        local pos = ent:GetPos() + Vector(0, 0, ent:OBBCenter().z + (elapsed * 10))
         
         -- 计算透明度，随着时间的推移逐渐变浅，先慢后快
         local alpha = math.max(255 * (1 - (elapsed / textDuration)), 0)  -- 计算透明度，最大255，最小0
         -- 调整透明度变化速度，使其先慢后快
         alpha = 255 * (1 - (elapsed / textDuration) ^ 2)
         
-        cam.Start3D2D(pos, Angle(0, LocalPlayer():EyeAngles().y - 90, 90), 0.1)
-            cam.IgnoreZ(true)
-            draw.SimpleText(levelUpText, "ofgui_eva", 0, 0, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-            cam.IgnoreZ(false)
-        cam.End3D2D()
+        local screenPos = pos:ToScreen()  -- 将世界坐标转换为屏幕坐标
+        draw.SimpleText(levelUpText, "ofgui_big", screenPos.x, screenPos.y, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     end
 
     -- 在HUD中绘制文本
-    hook.Add("PostDrawOpaqueRenderables", "DrawLevelUpText", function()
+    hook.Add("HUDPaint", "DrawLevelUpText", function()
         DrawLevelUpText()
         -- 在持续时间后移除钩子
         if CurTime() - startTime >= textDuration then
-            hook.Remove("PostDrawOpaqueRenderables", "DrawLevelUpText")
+            hook.Remove("HUDPaint", "DrawLevelUpText")
         end
     end)
 end)
