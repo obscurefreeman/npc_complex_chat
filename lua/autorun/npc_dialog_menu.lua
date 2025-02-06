3086226161b545da9f5070629c0a01a26f060c53
晦涩弗里曼 <1093048022@qq.com>
2025-02-06 19:37:00 +0800

    测试对话菜单

--- lua/autorun/npc_dialog_menu.lua
+++ lua/autorun/npc_dialog_menu.lua
@@ -9,6 +9,9 @@ if CLIENT then
             "leave"
         }
 
+        local npcs = GetAllNPCsList()
+        local npcIdentity = npcs[npc:EntIndex()]
+
         -- 从全局变量获取对话文本
         local playerTalkOptions = {}
         if GLOBAL_OFNPC_DATA.playerTalks then
@@ -42,36 +45,41 @@ if CLIENT then
             surface.DrawRect(0, 0, w, h)
         end
 
-        -- 左上角显示玩家模型上半身
         local playerModelPanel = vgui.Create("DModelPanel", leftPanel)
         playerModelPanel:SetModel(LocalPlayer():GetModel())
         playerModelPanel:Dock(TOP)
         playerModelPanel:SetHeight(400 * OFGUI.ScreenScale)
         function playerModelPanel:LayoutEntity( Entity ) return end
 
-        -- 右上角显示NPC模型上半身
         local npcModelPanel = vgui.Create("DModelPanel", rightPanel)
         npcModelPanel:SetModel(npc:GetModel())
         npcModelPanel:Dock(TOP)
         npcModelPanel:SetHeight(400 * OFGUI.ScreenScale)
         function npcModelPanel:LayoutEntity( Entity ) return end
 
-
         -- 下方中间区域存放对话选项
         local scrollPanel = vgui.Create("OFScrollPanel", frame)
         scrollPanel:SetHeight(400 * OFGUI.ScreenScale)
         scrollPanel:Dock(BOTTOM)
 
+        local messagelPanel = vgui.Create("OFScrollPanel", frame)
+        messagelPanel:Dock(FILL)
+
+        local message = vgui.Create("OFMessage", messagelPanel)
+        message:SetHeight(80 * OFGUI.ScreenScale)
+        message:Dock(TOP)
+        message:DockMargin(4, 4, 4, 4)
+        message:SetName(L(npcIdentity.name))
+        message:SetText("2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222")
+
         for _, option in ipairs(playerTalkOptions) do
-            local npcs = GetAllNPCsList()
-            local npcIdentity = npcs[npc:EntIndex()]
             local translatedOption = L(option):gsub("/name/", L(npcIdentity.name))
 
             -- 创建按钮并添加到ScrollPanel
             local button = vgui.Create("OFChatButton", scrollPanel)
             button:SetChatText(translatedOption)
             button:Dock(TOP)
-            button:DockMargin(4, 4, 4, 4) -- 添加按钮之间的间距
+            button:DockMargin(4, 4, 4, 4)
             button:SetIcon("ofnpcp/chaticons/chat.png")
             button:SetHoveredColor(Color(100, 255, 100))
             button:SetTall(50 * OFGUI.ScreenScale) -- 设置按钮的高度
