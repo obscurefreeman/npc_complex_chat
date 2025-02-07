8f375cccda472d223a15546c70029f4381ec0988
晦涩弗里曼 <1093048022@qq.com>
2025-02-07 12:55:00 +0800

    补充

--- lua/autorun/server/of_npctalk_kill.lua
+++ lua/autorun/server/of_npctalk_kill.lua
@@ -3,7 +3,7 @@ hook.Add("OnNPCKilled", "NPCTalkKill", function(victim, attacker, inflictor)
     if not (IsValid(attacker) and attacker:IsNPC()) then return end
     
     -- 检查是否正在对话
-    if NPCTalkManager:IsNPCTalking(attacker) then return end
+    if NPCTalkManager:IsNPCTalking(npc) or NPCTalkManager:IsNPCChating(npc) then continue end
     
     -- 获取NPC身份信息
     local identity = OFNPCS and OFNPCS[attacker:EntIndex()]
