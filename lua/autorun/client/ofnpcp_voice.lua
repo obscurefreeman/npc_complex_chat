c3a1e9b9dbff380d8d5b0451ce474e3568ef985b
晦涩弗里曼 <1093048022@qq.com>
2025-03-12 15:29:45 +0800

    修改格式

--- lua/autorun/client/ofnpcp_voice.lua
+++ lua/autorun/client/ofnpcp_voice.lua
@@ -13,24 +13,28 @@ end
 function PlayNPCDialogVoice(npc, text)
     local encodedText = UrlEncode(text)
 
-    local url = "http://124.222.73.18:8300/tts?text=" .. encodedText
-
-    http.Fetch(url, function(response)
-        -- 解析返回的 JSON 响应
-        local data = util.JSONToTable(response)
-        if data and data.audio_url then
-            sound.PlayURL(data.audio_url, "mono", function(station)
-                if IsValid(station) then
-                    station:SetPos(npc:GetPos())
-                    station:SetVolume(5.0)
-                    station:Play()
-                end
-            end)
-        else
-            print("Failed to get audio URL.")
+    HTTP({
+        url = "http://124.222.73.18:8300/tts?text=" .. encodedText,
+        method = "get",
+        headers = {
+            ["Authorization"] = "Bearer "
+        },
+        success = function(code, body, headers)
+            local data = util.JSONToTable(body)
+            if data and data.audio_url then
+                sound.PlayURL(data.audio_url, "mono", function(station)
+                    if IsValid(station) then
+                        station:SetPos(npc:GetPos())
+                        station:SetVolume(5.0)
+                        station:Play()
+                    end
+                end)
+            else
+                print("Failed to get audio URL.")
+            end
+        end,
+        failed = function(err)
+            print("HTTP request failed: " .. (err or "unknown error"))
         end
-    end, function(error)
-        -- 错误处理
-        print("HTTP request failed: " .. error)
-    end)
+    })
 end
