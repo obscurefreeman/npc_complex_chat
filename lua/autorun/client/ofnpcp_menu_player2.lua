a5b90b5867c7885ec31597977a6ee55ad6a067b9
晦涩弗里曼 <1093048022@qq.com>
2025-08-09 23:04:50 +0800

    拆分成两个function

--- lua/autorun/client/ofnpcp_menu_player2.lua
+++ lua/autorun/client/ofnpcp_menu_player2.lua
@@ -1,54 +1,54 @@
 -- CreateClientConVar("of_garrylord_player2_enable", "0", true, true, "", 0, 1)
 
 function OFNPCP_SetUpPlayer2Menu(player2Menu)
+
+	local CLIENT_ID = "0198804b-941a-71e0-8b49-37e15b1b2dd8"
+	local API_BASE_URL = "https://api.player2.game/v1"
+
+	-- 读取现有设置
+	local aiSettings = {}
+	local rawSettings = file.Read("of_npcp/ai_settings.txt", "DATA")
+	if rawSettings then
+		aiSettings = util.JSONToTable(rawSettings) or {}
+	end
+	-- 如果不存在Player2设置，则初始化默认设置，暂时不保存，只是呆在这里
+	if not aiSettings["player2"] then
+		aiSettings["player2"] = {
+			url = API_BASE_URL,
+			key = "",
+			model = "player2",
+			temperature = 1,
+			max_tokens = 500,
+			device = ""
+		}
+	end
+
+	-- 辅助函数：保存API设置
+	local function save_settings()
+		-- 确保目录存在
+		if not file.IsDir("of_npcp", "DATA") then
+			file.CreateDir("of_npcp")
+		end
+
+		-- 保存设置
+		file.Write("of_npcp/ai_settings.txt", util.TableToJSON(aiSettings))
+		RunConsoleCommand("of_garrylord_provider", "player2")
+	end
+
 	local player2Panel = vgui.Create("OFScrollPanel", player2Menu)
 	player2Panel:DockMargin(4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale)
 	player2Panel:Dock(FILL)
 
-	local function device_flow()
-		-- 常量定义
-		local CLIENT_ID = "0198804b-941a-71e0-8b49-37e15b1b2dd8"
-		local API_BASE_URL = "https://api.player2.game/v1"
+	-- 创建设备码输入框
+	local deviceCodeEntry = OFNPCPCreateControl(player2Panel, "OFTextEntry", {
+		SetValue = aiSettings.player2 and aiSettings.player2.device or "",
+		SetPlaceholderText = "Player2 设备码"
+	})
 
+	local function OFNPCP_Player2AuthDevice()
 		-- 显示进度通知
 		notification.AddProgress("Player2AuthDevice", "正在获取设备码")
 
-		-- 辅助函数：处理错误
-		local function handle_error(err, msg)
-			notification.AddLegacy(msg .. (err or ""), NOTIFY_ERROR, 5)
-			notification.Kill("Player2AuthDevice")
-			notification.Kill("Player2AuthAPI")
-		end
-
-		-- 辅助函数：保存API设置
-		local function save_api_settings(p2Key)
-			-- 确保目录存在
-			if not file.IsDir("of_npcp", "DATA") then
-				file.CreateDir("of_npcp")
-			end
-
-			-- 读取现有设置
-			local aiSettings = {}
-			local rawSettings = file.Read("of_npcp/ai_settings.txt", "DATA")
-			if rawSettings then
-				aiSettings = util.JSONToTable(rawSettings) or {}
-			end
-
-			-- 更新Player2设置
-			aiSettings["player2"] = {
-				url = API_BASE_URL,
-				key = p2Key,
-				model = "player2",
-				temperature = 1,
-				max_tokens = 500
-			}
-
-			-- 保存设置
-			file.Write("of_npcp/ai_settings.txt", util.TableToJSON(aiSettings))
-			RunConsoleCommand("of_garrylord_provider", "player2")
-		end
-
-		-- 第一步：获取设备码
 		HTTP({
 			url = API_BASE_URL .. "/login/device/new",
 			type = "application/json",
@@ -60,62 +60,81 @@ function OFNPCP_SetUpPlayer2Menu(player2Menu)
 				local responseData = util.JSONToTable(body)
 				
 				if responseData.error_description then
-					notification.AddLegacy("获取设备码失败：" .. responseData.error_description, NOTIFY_ERROR, 5)
+					notification.AddLegacy("申请设备码失败：" .. responseData.error_description, NOTIFY_ERROR, 5)
 				elseif responseData.deviceCode then
-					notification.AddLegacy("获取设备码成功", NOTIFY_GENERIC, 5)
-
-					notification.AddProgress("Player2AuthAPI", "正在获取免费Player2 API")
-
-					-- 第二步：使用设备码获取API Key
-					HTTP({
-						url = API_BASE_URL .. "/login/device/token",
-						type = "application/json",
-						method = "post",
-						body = util.TableToJSON({
-							client_id = CLIENT_ID,
-							device_code = responseData.deviceCode,
-							grant_type = 'urn:ietf:params:oauth:grant-type:device_code'
-						}),
-						
-						success = function(code, body, headers)
-							notification.Kill("Player2AuthAPI")
-							local responseData = util.JSONToTable(body)
-							
-							if responseData.error_description then
-								notification.AddLegacy("获取API失败：" .. responseData.error_description, NOTIFY_ERROR, 5)
-							elseif responseData.p2Key then
-								notification.AddLegacy("获取API成功，PLAYER2API已保存到本地，您现在可以使用它了", NOTIFY_GENERIC, 5)
-								save_api_settings(responseData.p2Key)
-							else
-								notification.AddLegacy("获取API失败，未知错误", NOTIFY_ERROR, 5)
-							end
-						end,
-						
-						failed = function(err)
-							notification.AddLegacy("获取API失败：" .. err, NOTIFY_ERROR, 5)
-							notification.Kill("Player2AuthAPI")
-						end
-					})
+					notification.AddLegacy("申请设备码成功", NOTIFY_GENERIC, 5)
+					deviceCodeEntry:SetValue(responseData.deviceCode)  -- 将设备码显示在输入框中
+					aiSettings.player2.device = responseData.deviceCode
+					save_settings()
 				else
-					notification.AddLegacy("获取设备码失败，未知错误", NOTIFY_ERROR, 5)
-					notification.Kill("Player2AuthAPI")
+					notification.AddLegacy("申请设备码失败，未知错误", NOTIFY_ERROR, 5)
 				end
 			end,
 			
 			failed = function(err)
-				notification.AddLegacy("获取API失败：" .. err, NOTIFY_ERROR, 5)
+				notification.AddLegacy("申请设备码失败：" .. err, NOTIFY_ERROR, 5)
 				notification.Kill("Player2AuthDevice")
 			end
 		})
 	end
 
-	-- 创建获取API Key的按钮
-	local getKeyButton = vgui.Create("OFButton", player2Panel)
-	getKeyButton:Dock(TOP)
-	getKeyButton:SetTall(40 * OFGUI.ScreenScale)
-	getKeyButton:DockMargin(4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale)
-	getKeyButton:SetText(ofTranslate("ui.player2.get_key"))
-	getKeyButton.DoClick = function()
-		device_flow()
+	local getDeviceKeyButton = OFNPCPCreateControl(player2Panel, "OFButton", {
+		SetText = (ofTranslate("ui.player2.get_device_key"))
+	})
+	getDeviceKeyButton.DoClick = function()
+		OFNPCP_Player2AuthDevice()
+	end
+
+	-- 创建API输入框
+	local apiCodeEntry = OFNPCPCreateControl(player2Panel, "OFTextEntry", {
+		SetValue = aiSettings.player2 and aiSettings.player2.key or "",
+		SetPlaceholderText = "Player2 API"
+	})
+
+	local function OFNPCP_Player2AuthAPI()
+		-- 显示进度通知
+		notification.AddProgress("Player2AuthAPI", "正在获取API")
+
+		HTTP({
+			url = API_BASE_URL .. "/login/device/token",
+			type = "application/json",
+			method = "post",
+			body = util.TableToJSON({
+				client_id = CLIENT_ID,
+				device_code = aiSettings.player2 and aiSettings.player2.device or "",
+				grant_type = 'urn:ietf:params:oauth:grant-type:device_code'
+			}),
+			
+			success = function(code, body, headers)
+				notification.Kill("Player2AuthAPI")
+				local responseData = util.JSONToTable(body)
+
+				print("Response Table:")
+				PrintTable(responseData)
+				PrintTable(aiSettings.player2)
+				
+				if responseData.error_description then
+					notification.AddLegacy("获取API失败：" .. responseData.error_description, NOTIFY_ERROR, 5)
+				elseif responseData.p2Key then
+					notification.AddLegacy("获取API成功，PLAYER2API已保存到本地，您现在可以使用它了", NOTIFY_GENERIC, 5)
+					aiSettings.player2.device = responseData.p2Key
+					save_settings()
+				else
+					notification.AddLegacy("获取API失败，未知错误", NOTIFY_ERROR, 5)
+				end
+			end,
+			
+			failed = function(err)
+				notification.AddLegacy("获取API失败：" .. err, NOTIFY_ERROR, 5)
+				notification.Kill("Player2AuthAPI")
+			end
+		})
+	end
+
+	local getapiButton = OFNPCPCreateControl(player2Panel, "OFButton", {
+		SetText = (ofTranslate("ui.player2.get_api"))
+	})
+	getapiButton.DoClick = function()
+		OFNPCP_Player2AuthAPI()
 	end
 end
